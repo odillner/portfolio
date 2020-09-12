47843da34900e5bac1b04ec7ed522fdf9481e7dd
odillner <o.dillner@gmail.com>
2020-09-12 13:57:09 +0200

    sidebar && icons implemented

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,46 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledNavItem = styled.div`
-  height: 70px;
-  width: 75px; /* width must be same size as NavBar to center */
-  text-align: center; /* Aligns <a> inside of NavIcon div */
-  margin-bottom: 0;   /* Puts space between NavItems */
-  a {
-    font-size: 2.7em;
-    color: ${(props) => props.active ? "white" : "#9FFFCB"};
-    :hover {
-      opacity: 0.7;
-      text-decoration: none; /* Gets rid of underlining of icons */
-    }  
-  }
-`
+import icons from '../icons'
 
-const StyledSideNav = styled.div`
-  position: fixed;     /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
-  height: 100%;
-  width: 75px;     /* Set the width of the sidebar */
-  z-index: 1;      /* Stay on top of everything */
-  top: 0em;      /* Stay at the top */
-  background-color: #222; /* Black */
-  overflow-x: hidden;     /* Disable horizontal scroll */
-  padding-top: 10px;
+const NavItemWrapper = styled.div`
+
 `
-const NavItem = () => {
+const NavItem = ({type, active}) => {
+    const Icon = (type) ? icons[type] : null
+
     return (
-
+        <NavItemWrapper>
+            <Icon/>
+        </NavItemWrapper>
     )
 }
 
-const SideNav = () => {
-    return (
-        <StyledSideNav></StyledSideNav>
-    )
-}
+const MainItems = styled.div`
+
+`
+const BottomItems = styled.div`
+    display: block;
+    padding: 0;
+    padding-bottom: 0px;
+    list-style: none;
+    text-align: center;
+    margin: 0;
+    padding-bottom: 20px;
+`
+const SideNav = styled.div`
+    background: #181818;
+    width: 100px;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    z-index: 3;
+    min-height: 500px;
+`
+
 
 const Sidebar = () => {
     return (
-        <SideNav></SideNav>
+        <SideNav>
+            <MainItems>
+                <NavItem type='GitHub'></NavItem>
+                <NavItem type='GitHub'></NavItem>
+            </MainItems>
+            <BottomItems>
+                <NavItem type='GitHub'></NavItem>
+                <NavItem type='GitHub'></NavItem>
+            </BottomItems>
+        </SideNav>
     )
 }
 
